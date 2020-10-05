4a338e336b3fc502303e1128feda380e7912d603
zxnii <61130784+Zxnii@users.noreply.github.com>
2020-10-05 10:47:33 -0600

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const process = require("process")
-
-process.stdout.write("What's your name? ")
-
-process.stdin.on("data", (chunk) => {
-    chunk = chunk.toString()
-    process.stdout.write(`Hi, ${chunk}`)
-    process.exit(0)
-})
